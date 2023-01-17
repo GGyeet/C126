8bc8c885880aef96b3eba9681fdd81a96ceab50a
GG <105051795+GGyeet@users.noreply.github.com>
2023-01-17 16:35:41 +1000

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-function startClassification(){
-    navigator.mediaDevices.getUserMedia({audio:true});
-    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/8T_3DuJX9/model.json',modelReady);
-}
